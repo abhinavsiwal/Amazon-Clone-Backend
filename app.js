aeac474ac1e7003f5086db7c68bd9eb7fc643bf5
abhinavsiwal <abhinavsiwal@gmail.com>
2021-10-14 14:06:01 +0530

    MongoDb Connected

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
-module.exports = app;
+//Import all the routes
+const products = require('./routes/product')
+
+app.use('/api/products',products);
+
+module.exports = app; 
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const app = require('./app');
-
+const connectDatabase = require('./configs/database')
 const dotenv = require('dotenv');
 
 //Setting Up Config Files
 dotenv.config({path:'./config/config.env'})
 
+//Connecting to Database
+connectDatabase()
+
 app.listen(process.env.PORT,()=>{
     console.log(`Server started on Port : ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
-})
+}) 
