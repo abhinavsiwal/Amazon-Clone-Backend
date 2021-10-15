bed34254d15a98a0722f031badaabfddbe8d1f79
abhinavsiwal <abhinavsiwal@gmail.com>
2021-10-15 17:42:01 +0530

    Login and Register User

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ app.use(express.json());
 
 //Import all the routes
 const products = require('./routes/product')
+const auth = require('./routes/auth')
 
 app.use('/api/',products);
-
+app.use('/api/',auth)
 module.exports = app; 
---
+++ controllers/auth-controllers.js
@@ -0,0 +1,105 @@
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+//Register a User
+exports.registerUser = async (req, res, next) => {
+  const { name, email, password } = req.body;
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ message: "Signing up falied, Please try again later" });
+  }
+  if (existingUser) {
+    return res
+      .status(500)
+      .json({ message: "User already exists. Please try to login" });
+  }
+
+  //-----Bcrypt Password-----
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    console.log(err);
+  }
+
+  const createdUser = new User({
+    name,
+    email,
+    password: hashedPassword,
+    avatar: {
+      public_id: "IMG_2928-02_chtyat",
+      url: "https://res.cloudinary.com/abhinavsiwal/image/upload/v1634277630/IMG_2928-02_chtyat.jpg",
+    },
+  });
+  try {
+    await createdUser.save();
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ message: "Signing up falied, Please try again later" });
+  }
+  let token;
+  try {
+    token = jwt.sign(
+      {
+        id: this._id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+  } catch (err) {
+      console.log(err)
+  }
+  res.status(201).json({
+    success: true,
+    token,
+  });
+};
+
+// Login user
+
+exports.loginUser = async (req, res, next) => {
+  const { email, password } = req.body;
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email }).select("+password");
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ message: "Login failed, Please try again later." });
+  }
+  if (!existingUser) {
+    return res.status(401).json({ message: "User not Found." });
+  }
+  let isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ message: "Login failed, Please try again later." });
+  }
+  if (!isValidPassword) {
+    return res.status(401).json({ message: "Incorrect Password" });
+  }
+  let token;
+  try {
+    token = jwt.sign({ userId: existingUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+  } catch (err) {
+      console.log(err);
+  }
+  res.status(200).json({
+      success:true,
+      token,
+  })
+};
