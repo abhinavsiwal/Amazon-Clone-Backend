a54dd8d669e29e46b1c4f164957775c8f8ff77d7
abhinavsiwal <abhinavsiwal@gmail.com>
2021-10-16 16:56:04 +0530

    Generating forgot password token

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator'); 
-
+const crypto=require('crypto');
 const userSchema = new mongoose.Schema({
     name:{
         type:String,
@@ -41,5 +41,18 @@ const userSchema = new mongoose.Schema({
     resetPasswordExpire:Date,   
 
 })
+//Generate Password Reset Token
+userSchema.methods.getResetPasswordToken=function(){
+    //Generate token
+    const resetTokan=crypto.randomBytes(20).toString('hex');
+
+    // Hash and set to resetPasswordToken
+    this.resetPasswordToken=crypto.createHash('sha256').update(resetToken).digest('hex');
+
+    // Set token expire time 
+    this.resetpasswordExpire=Date.now() + 30*60*1000;
+
+    return resetToken;
+}
 
 module.exports= mongoose.model('User',userSchema);
